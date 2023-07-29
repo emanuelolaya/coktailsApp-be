dc06d6ac5e3efb5f4d77a2182ca98012ab901d6f
Andres Felipe Ramirez Ocaña <aramirez@indigo.tech>
2023-07-29 16:23:09 -0500

    add openai and generate random cocktails

--- index.js
+++ index.js
@@ -1,5 +1,46 @@
 const express = require("express");
 const oracledb = require("oracledb");
+const { OpenAI } = require("openai");
+
+const apiOpenAI = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY
+});
+
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+function showOpenAIError(error) {
+  console.error(`OpenAi Error: 
+    status=${error.status}, 
+    message=${error.message}, 
+    code=${error.code}, 
+    type=${error.type} `);
+}
+
+const randomCocktail = async () => {
+  try {
+    const result = await apiOpenAI.chat.completions.create({
+      model: 'gpt-3.5-turbo',
+      messages: [{
+        role: 'system',
+        content: 'Eres un experto mixólogo virtual con acceso a una amplia base de datos de ingredientes y recetas de cócteles.'
+      }, {
+        role: 'user',
+        content: 'Genera un cóctel aleatorio con sus ingredientes y su preparación. Puedes incluir cualquier combinación de licores, frutas, jarabes u otros ingredientes comunes en cócteles. Asegúrate de proporcionar los pasos detallados para la preparación del cóctel.'
+      }]
+    });
+    return result?.choices[0]?.message?.content;
+  } catch (error) {  
+    if (error instanceof OpenAI.APIError) {
+      showOpenAIError(error)
+    } else {
+      console.log(error);
+    }
+  }
+}
 
 const dbConfig = {
   user: "ADMIN",
@@ -14,7 +55,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 oracledb.getConnection(dbConfig).then((connection) => {
-  console.log("connection", connection);
+  //console.log("connection", connection);
 });
 
 app.listen(PORT, () => {
@@ -23,4 +64,9 @@ app.listen(PORT, () => {
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hola desde el servidor!" });
+});
+
+app.get("/api/cocktail/random", async (req, res) => {
+  const cocktail = await randomCocktail();
+  res.json({ cocktail });
 });
