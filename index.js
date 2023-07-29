ceb254c083a69e8122afa1c57cecd98944842854
Emanuel Olaya <emanuel.olaya@connexian.com>
2023-07-29 11:06:52 -0500

    add nodemon and oracle db conection sample

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const express = require("express");
-const neo4j = require('neo4j-driver')
+const oracledb = require("oracledb");
+
+const dbConfig = {
+  user: "ADMIN",
+  password: "fBj32T6tzxmbt5x",
+  connectString:
+    "(description= (retry_count=20)(retry_delay=3)(address=(protocol=tcps)(port=1522)(host=adb.us-ashburn-1.oraclecloud.com))(connect_data=(service_name=g34127bafa0e1a3_e2jzcbmwmjjk2fiz_high.adb.oraclecloud.com))(security=(ssl_server_dn_match=yes)))",
+  externalAuth: false,
+};
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+oracledb.getConnection(dbConfig).then((connection) => {
+  console.log("connection", connection);
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
 
 app.get("/api", (req, res) => {
-    res.json({ message: "Hola desde el servidor!" });
+  res.json({ message: "Hola desde el servidor!" });
 });
